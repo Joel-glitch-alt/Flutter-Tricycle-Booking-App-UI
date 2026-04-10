16ca907e5b455d550133c5499aa0a9749d25ae47
Joel-glitch-alt <joeladdition@gmail.com>
2026-04-10 16:27:49 +0000

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tricycle_booking_app/Pages/callScreen.dart';
 import 'package:tricycle_booking_app/Pages/forgotpassword.dart';
-//import 'package:tricycle_booking_app/Pages/google.dart';
 import 'package:tricycle_booking_app/Pages/landingPage.dart';
 import 'package:tricycle_booking_app/Pages/loginPage.dart';
 import 'package:tricycle_booking_app/Pages/orderRide.dart';
-//import 'package:tricycle_booking_app/Pages/paymentSelection.dart';
 import 'package:tricycle_booking_app/Pages/riderChat.dart';
 import 'package:tricycle_booking_app/Pages/signUp.dart';
 import 'package:tricycle_booking_app/Pages/userProfile.dart';
+import 'package:tricycle_booking_app/providers/auth_provider.dart';
+import 'package:tricycle_booking_app/services/api_service.dart';
+import 'package:tricycle_booking_app/services/auth_service.dart';
+import 'package:tricycle_booking_app/services/storage_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,23 +22,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowMaterialGrid: false,
-      title: 'Tricycle Booking',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const LandingPage(),
-        '/LoginPage': (context) => LoginPage(),
-        '/SignUpPage': (context) => SignUpPage(),
-        '/ForgotPasswordScreen': (context) => const ForgotPasswordScreen(),
-        '/OrderRide': (context) => const TricycleBookingPage(),
-        '/CallScreen': (context) => const CallScreen(),
-        //'/PaymentSelection': (context) => const PaymentSelectionScreen(),
-        '/RiderChat': (context) => const RiderChatScreen(),
-        //'/UserProfile': (context) => const UserProfileScreen(),
-        '/UserProfileScreen': (context) => UserProfileScreen(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) {
+            final storageService = StorageService();
+            final apiService =
+                ApiService(storageService); // ← pass StorageService
+            return AuthProvider(
+              AuthService(apiService),
+              storageService, // ← reuse same instance
+            );
+          },
+        )
+      ],
+      child: MaterialApp(
+        debugShowMaterialGrid: false,
+        title: 'Tricycle Booking',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const LandingPage(),
+          '/LoginPage': (context) => LoginPage(),
+          '/SignUpPage': (context) => SignUpPage(),
+          '/ForgotPasswordScreen': (context) => const ForgotPasswordScreen(),
+          '/OrderRide': (context) => const TricycleBookingPage(),
+          '/CallScreen': (context) => const CallScreen(),
+          '/RiderChat': (context) => const RiderChatScreen(),
+          '/UserProfileScreen': (context) => UserProfileScreen(),
+        },
+      ),
     );
   }
 }
